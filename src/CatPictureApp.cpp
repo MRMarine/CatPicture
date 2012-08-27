bd4b154e34e077f4dadda7d96c4d027b5403d89b
Matthew Marine <marinemr@muohio.edu>
2012-08-27 00:45:11 -0400

    Initial Commit
    I copied my code from Hello World when I realized I should change the
    name to CatPictureApp from HelloWorldApp.  The only problem should be
    making sure that cinder is in the right directory.
    
    I'm getting a commit failed message, so I'm going to break up the commit
    into a few parts.

---
+++ src/CatPictureApp.cpp
@@ -0,0 +1,48 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class CatPictureApp : public AppBasic {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+  private:
+	float red, green, blue;
+};
+
+void CatPictureApp::setup()
+{
+}
+
+void CatPictureApp::mouseDown( MouseEvent event )
+{
+	// change the screen color by checking the mouse position in the window
+	// and dividing by the height/width to get a ratio from 0 to 1 for red and green
+	
+	// viewport saves as an array [0, 0, width, height] if I'm not mistaken.
+	GLint window_viewport[4];
+
+	glGetIntegerv( GL_VIEWPORT, window_viewport );
+	
+	red = (float)(event.getX()) / window_viewport[2];
+	green = (float)(event.getY()) / window_viewport[3];
+	// set the blue ratio to a random float between 0 and 1.
+	blue = (float)(rand() % 10 / 10.0);
+}
+
+void CatPictureApp::update()
+{
+}
+
+void CatPictureApp::draw()
+{
+	// clear out the window to the value determined by the RGB set in mouseDown( MouseEvent )
+	gl::clear( Color( red, green, blue ) ); 
+}
+
+CINDER_APP_BASIC( CatPictureApp, RendererGl )
